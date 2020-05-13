68c57b5bf29cf902a30b9e2e7f89e34212688e04
Nelson Duarte <nelson0204@gmail.com>
2020-05-13 22:24:39 +0100

    13-05-2020

---
+++ aula94-Exports/src/index.js
@@ -0,0 +1,14 @@
+//import {nome as nome2,sobrenome,idade,soma} from './modulo1';
+// import {nome as nome2 ,sobrenome,idade,soma, Pessoa} from './modulo1';
+// import * as MeuModulo from './modulo1';
+import qualquerNome,{nome, sobrenome, idade,soma} from './modulo1';
+
+console.log(qualquerNome(5,5));
+
+// const nome = 'João';
+
+ console.log(nome, sobrenome, idade); 
+// console.log(soma(5,5));
+
+// const p1 = new Pessoa('Nelson','Duarte');
+// console.log(p1);
---
+++ aula94-Exports/src/modulo1.js
@@ -0,0 +1,24 @@
+ const nome = 'Nelson';
+ const sobrenome = 'Duarte';
+ const idade = 30;
+const cpf = 'dsafd';
+
+
+function soma(x,y){
+    return x + y;
+}
+
+export default (x,y) => x * y;
+
+
+
+export {nome , sobrenome, idade, soma};
+// export class Pessoa{
+//     constructor(nome,sobrenome){
+//         this.nome = nome;
+//         this.sobrenome = sobrenome;
+//     }
+// }
+
+//export { nome, sobrenome, idade, soma };
+//export { nome2, sobrenome, idade, soma };
